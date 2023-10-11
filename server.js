061a10da95394cd816fc7c71a7a446144d93673b
Wen Sun <114129037+WenSun09@users.noreply.github.com>
2023-10-10 20:41:18 -0700

    Create node.js.yml (#1)
    * Create node.js.yml
    
    * add test
    
    * update mysql pw
    
    * update db host

--- server.js
+++ server.js
@@ -1,40 +1,9 @@
-const express = require('express');
-const health = require('./health');
+const { app, db } = require('./app');
 const init = require('./init');
-const db = require('./models');
-const auth = require('./auth');
-const assignment = require('./assignment');
 
-// Method Not Allowed
-const methodNotAllowed = (req, res, next) => res.status(405).send();
-
-const app = express();
-init.initDatabase();
-
-app.use(function (req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    next();
+app.listen(8080, async () => {
+    await init.initDatabase();
+    await db.sequelize.sync();
+    await init.initAccount(db);
+    console.log('App is listening on port 8080.');
 });
-
-db.sequelize.sync()
-    .then(() => {
-        console.log("Synced db.");
-        init.initAccount(db);
-    })
-    .catch((err) => {
-        console.log("Failed to sync db: " + err.message);
-    });
-
-app.route(`/healthz`)
-    .get(health.getHealthz)
-    .all(methodNotAllowed);
-
-app.route(`/v1/assignments/:id?`)
-    .get(auth.authentication(db), assignment.getAssignments(db))
-    .post(auth.authentication(db), assignment.postAssignments(db))
-    .delete(auth.authentication(db), assignment.delAssignmentByid(db))
-    .put(auth.authentication(db), assignment.putAssignmentByid(db))
-    .all(methodNotAllowed);
-
-app.listen(8080, () => console.log('App is listening on port 8080.'));
---
+++ test/app.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest')
+const { app, db } = require('../app')
+const init = require('../init');
+
+const server = app.listen(8080, () => { console.log('App is listening on port 8080.'); });
+
+beforeAll(async () => {
+    await init.initDatabase();
+    await db.sequelize.sync();
+    await init.initAccount(db);
+})
+
+afterAll(async () => {
+    await db.sequelize.close();
+    await server.close();
+});
+
+describe('Get Healthz', () => {
+    it('get healthz should return 200', (done) => {
+        request(app)
+            .get('/healthz')
+            .send()
+            .end(function (err, res) {
+                expect(res.statusCode).toEqual(200);
+                if (err) {
+                    throw err;
+                }
+                done();
+            });
+    })
+});
