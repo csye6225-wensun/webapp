487fc39864f65f740146c673d22b181b16ce10d4
Wen Sun <114129037+WenSun09@users.noreply.github.com>
2023-10-13 00:35:52 -0700

    A03fix (#6)
    * Create node.js.yml (#1)
    
    * Create node.js.yml
    
    * add test
    
    * update mysql pw
    
    * update db host
    
    * fix auth base64, fix param json
    
    * fix test
    
    * fix test db

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const health = require('./health');
 const db = require('./models');
 const auth = require('./auth');
@@ -8,6 +10,7 @@ const assignment = require('./assignment');
 const methodNotAllowed = (req, res, next) => res.status(405).send();
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
@@ -19,7 +22,7 @@ app.route(`/healthz`)
     .get(health.getHealthz)
     .all(methodNotAllowed);
 
-app.route(`/v1/assignments/:id?`)
+app.route(`/v1/assignments/`)
     .get(auth.authentication(db), assignment.getAssignments(db))
     .post(auth.authentication(db), assignment.postAssignments(db))
     .delete(auth.authentication(db), assignment.delAssignmentByid(db))
--- auth.js
+++ auth.js
@@ -5,28 +5,32 @@ const authentication = (db) => {
         const authheader = req.headers.authorization;
 
         if (!authheader) {
-            return res.status(401).send("");
+            return res.status(401).json();
         }
 
-        const auth = authheader.split(':');
-        const email = auth[0];
-        const password = auth[1];
-
-        db.accounts.findOne({ where: { email: email } }).then((user) => {
-            if (!user) return res.status(401).send("");
-            bcrypt.compare(password, user.password, function (err, compareRes) {
-                if (err) {
-                    return res.status(401).send("");
-                } else if (!compareRes) {
-                    return res.status(401).send("");
-                } else {
-                    req.userid = user.id;
-                    next();
-                }
-            });
-        }).catch((err) => {
-            return res.status(401).send("");
-        })
+        try {
+            const authBuf = atob(authheader);
+            const auth = authBuf.split(' ')[1].split(':');
+            const email = auth[0];
+            const password = auth[1];
+            db.accounts.findOne({ where: { email: email } }).then((user) => {
+                if (!user) return res.status(401).json();
+                bcrypt.compare(password, user.password, function (err, compareRes) {
+                    if (err) {
+                        return res.status(401).json();
+                    } else if (!compareRes) {
+                        return res.status(401).json();
+                    } else {
+                        req.userid = user.id;
+                        next();
+                    }
+                });
+            }).catch((err) => {
+                return res.status(401).json();
+            })
+        } catch (err) {
+            return res.status(400).json();
+        }
     }
 }
 
