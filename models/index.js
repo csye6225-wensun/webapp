413aedb86d28b77b568ff9cb9e148dfae5244780
ymniam <sw199599@gmail.com>
2023-10-27 01:09:36 -0700

    db read env

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const dbConfig = require('../config/db.config');
-
-const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
+const sequelize = new Sequelize(process.env.DATABASE_DB, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
     logging: false
 });
 
